9fe7246841c3c8eae33a7163b95c20436eca44f3
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2023-05-16 09:15:31 +0300

    цыклы программка

--- js/script.js
+++ js/script.js
@@ -1,31 +1,62 @@
 'use strict';
 
 
-let numbersOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
 
 
-const personalMovieDB = {
-	count : '',
-	movies:{},
-	actors:{},
-	genres:[],
-	privat:false
+//const  numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+
+/* metca: for(;;){
+	const  numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','');
+	const personalMovieCount = numberOfFilms;
+	if(numberOfFilms.length === 0 ){
+		alert('Строка не может быть пустой!');
+		continue metca;
+	}else if(numberOfFilms.length > 50){
+		alert('Строка не может быть длиннее чем 50 символов');
+		continue metca;
+	}else if(+personalMovieCount<= 10){
+		alert('Просмотрено мало фильмов');
+		break;
+	}else if(+personalMovieCount> 10 && +personalMovieCount <= 30){
+		alert('Вы класический зритель');
+		break;
+	}else if(+personalMovieCount > 30){
+		alert('Вы киноман');
+		break;
+	}else{
+		alert('Произошла ошибка');
+		break;
+	}	
+}
+
+alert('Выход из опросника по фильмам');
+ */
+
+const  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+let personalData ={
+	personalMovieCount:numberOfFilms,
+	movies:{
+
+	}
 };
-personalMovieDB['count'] = numbersOfFilms;
-
-let lastFilm =  prompt('last Film?','');
-let reiting =  prompt('reiting?','');
 
 
-personalMovieDB['movies'][lastFilm]  = reiting ;
 
-let lastFilmNext = prompt('last Film Next?','');
-let reitingNext =  prompt('reitingNext?','');
+mt:for (;;) {
+	let a = prompt('Какие фильмы вы уже посмотрели?','');
+	let b =  prompt('Реитинг этих фильмов?','');
 
-personalMovieDB['movies'][lastFilmNext]  = reitingNext ;
+	if(a != null && b != null && a!= '' && b != '' && a.length < 50 ){
+		personalData.movies[a] = b;
+		alert('Done');
+		break;
+	}else{
+		alert('Error');
+		continue mt;
+	}
+}
 
-
-console.log(personalMovieDB);
-
-
-alert(personalMovieDB.movies +' '+personalMovieDB.movies.lastFilm);
+if(personalData.personalMovieCount < 10){
+	alert('<10');
+}
+console.log(personalData);
