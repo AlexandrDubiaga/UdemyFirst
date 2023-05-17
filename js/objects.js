81620851d2d3c13976f0d7004cd6ccc257dae36a
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2023-05-17 17:09:35 +0300

    objects-2

--- js/objects.js
+++ js/objects.js
@@ -7,20 +7,32 @@ const options = {
 	colors:{
 		border:'black',
 		bg:'red'
+	},
+	maketest:function(){
+		console.log('Test');
 	}
 };
 
-
+const{border, bg} = options.colors;
 
 
 
 for (let key in options) {
 	if(typeof(options[key])==='object'){
 		for (let i in options[key]){
-			console.log(`Ключ: ${i} - Значение: ${options[key][i]}`);
+			//console.log(`Ключ: ${i} - Значение: ${options[key][i]}`);
 		}
 	}else{
-		console.log(`Ключ: ${key} - Значение: ${options[key]}`);
+		//console.log(`Ключ: ${key} - Значение: ${options[key]}`);
 	}
 
 }
+
+
+
+console.log(Object.keys(options).length);
+
+
+
+options.maketest();
+
